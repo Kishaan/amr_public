b1e24b0f2d71ea6f193d66dd0481a6f0859e56ac
Alexander Hagg <info@haggdesign.de>
2013-10-20 14:34:49 +0200

    added code for differential velocity controller as it is no longer part of the assignment

--- amr_navigation/src/diff_velocity_controller.cpp
+++ amr_navigation/src/diff_velocity_controller.cpp
@@ -23,18 +23,46 @@ bool DiffVelocityController::isTargetReached() const
 
 Velocity DiffVelocityController::computeVelocity(const Pose& actual_pose)
 {
-  //========================= YOUR CODE HERE =========================
-  // Instructions: compute the velocity based on the target pose and
-  //               the actual pose. Update the bool flags
-  //               linear_complete_ and angular_complete_ to indicate
-  //               whether a corresponding component of the movement
-  //               is done.
-  //
-  // Hint: use the static helper functions getDistance() and
-  //       getShortestAngle() implemented in the VelocityController
-  //       class.
+  // Displacement and orientation to the target in world frame
+  double dx = target_pose_.x - actual_pose.x;
+  double dy = target_pose_.y - actual_pose.y;
 
+  // Step 1: compute remaining distances
+  double linear_dist = getDistance(target_pose_, actual_pose);
+  double angular_dist = getShortestAngle(target_pose_.theta, actual_pose.theta);
 
-  //==================================================================
+  if (std::abs(linear_dist) < l_tolerance_ && std::abs(angular_dist) < a_tolerance_)
+  {
+    linear_complete_ = true;
+    angular_complete_ = true;
+    return Velocity();
+  }
+
+  if (std::abs(linear_dist) > l_tolerance_)
+    // We still need to drive to the target, therefore we first need to make
+    // sure that we are oriented towards it.
+    angular_dist = getShortestAngle(atan2(dy, dx), actual_pose.theta);
+
+  // Step 2: compute velocities
+  double linear_vel = 0.0;
+  double angular_vel = 0.0;
+
+  if (std::abs(linear_dist) > l_tolerance_)
+    linear_vel = std::abs(linear_dist) > 5 * l_tolerance_ ? l_max_vel_ : l_tolerance_;
+
+  if (std::abs(angular_dist) > a_tolerance_)
+    angular_vel = std::abs(angular_dist) > 5 * a_tolerance_ ? a_max_vel_ : a_tolerance_;
+
+  if (std::abs(angular_dist) > a_tolerance_ * 5)
+  {
+    // We need to rotate a lot, so stand still and rotate with max velocity.
+    return Velocity(0, 0, std::copysign(angular_vel, angular_dist));
+  }
+  else
+  {
+    // We need to rotate just a bit (or do not need at all), so it is fine to
+    // combine with linear motion if needed.
+    return Velocity(std::copysign(linear_vel, linear_dist), 0, std::copysign(angular_vel, angular_dist));
+  }
 }
 
